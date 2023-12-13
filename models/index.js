664c6654c64fdf01c12cc270923c9e2db042ded8
Evan <evanbennett83@gamil.com>
2023-12-13 09:54:23 -0500

    Specified foreign keys on one to many relationship and specified many to many relationship to not check for unique values.

--- models/index.js
+++ models/index.js
@@ -4,21 +4,31 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-Product.Category = Product.belongsTo(Category)
-
 // Categories have many Products
-// *Plural products to indicate one to many relationship
-Category.Products = Category.hasMany(Product)
+Category.Product = Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
+// Products belongsTo Category
+Product.Category = Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  
-})
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {through: ProductTag})
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+});
 
 module.exports = {
   Product,
