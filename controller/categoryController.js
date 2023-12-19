179843eaa7b755a94f6cb182cf9d1422d00af6b5
Evan <evanbennett83@gamil.com>
2023-12-19 09:17:05 -0500

    Added logic for all category routes to controller.

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -19,7 +19,53 @@ const category_details = async (req, res) => {
   }
 };
 
+const category_post = async (req, res) => {
+  try {
+    const { category_name: name } = req.body
+    if (!name) return res.status(400).json('Please enter a category name')
+
+    const newCategory = await Category.create({ category_name: name })
+    res.json(newCategory)
+  } catch (err) {
+    res.status(500).json(err.errors[0].message)
+  }
+};
+
+const category_update = async (req, res) => {
+  try {
+    const categoryId = req.params.id
+    const { category_name: categoryName } = req.body
+    if (!categoryName) return res.status(404).json('Please enter a category name to update')
+
+    const updateCategoryName = await Category.update({ category_name: categoryName }, {
+      where: {
+        id: categoryId
+      }
+    });
+    res.json(updateCategoryName)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+};
+
+const category_delete = async (req, res) => {
+  try {
+    const categoryId = req.params.id
+    const updateCategoryName = await Category.destroy({
+      where: {
+        id: categoryId
+      }
+    });
+    res.json(updateCategoryName)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+};
+
 module.exports = {
   category_index,
-  category_details
+  category_details,
+  category_post,
+  category_update,
+  category_delete
 }
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,47 +8,10 @@ router.get('/', categoryController.category_index);
 
 router.get('/:id', categoryController.category_details);
 
-router.post('/', async (req, res) => {
-  try {
-    const { category_name: name } = req.body
-    if (!name) return res.status(400).json('Please enter a category name')
+router.post('/', categoryController.category_post);
 
-    const newCategory = await Category.create({ category_name: name })
-    res.json(newCategory)
-  } catch (err) {
-    res.status(500).json(err.errors[0].message)
-  }
-});
+router.put('/:id', categoryController.category_update);
 
-router.put('/:id', async (req, res) => {
-  try {
-    const categoryId = req.params.id
-    const { category_name: categoryName } = req.body
-    if (!categoryName) return res.status(404).json('Please enter a category name to update')
-
-    const updateCategoryName = await Category.update({ category_name: categoryName }, {
-      where: {
-        id: categoryId
-      }
-    });
-    res.json(updateCategoryName)
-  } catch (err) {
-    res.status(500).json(err)
-  }
-});
-
-router.delete('/:id', async (req, res) => {
-    try {
-    const categoryId = req.params.id
-    const updateCategoryName = await Category.destroy({
-      where: {
-        id: categoryId
-      }
-    });
-    res.json(updateCategoryName)
-  } catch (err) {
-    res.status(500).json(err)
-  }
-});
+router.delete('/:id', categoryController.category_delete);
 
 module.exports = router;
